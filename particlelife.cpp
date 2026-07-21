2f495ee5a6798cec90fb1d2bb728935bd38d69d8
WarpedWartWars <91039320+WarpedWartWars@users.noreply.github.com>
2026-07-21 08:19:10 -0700

    hot particles

--- particlelife.cpp
+++ particlelife.cpp
@@ -75,11 +75,11 @@ SDL_AppResult SDL_AppIterate(void *appstate)
             const float dy = particlesa[a].y - particlesb[b].y;
             const float distance = std::sqrt(dx * dx + dy * dy);
             const SDL_FPoint direction = { dx / distance, dy / distance };
-            if (distance < 50.0f) {
-                pa_vel[a].x += direction.x * 10.0f / distance;
-                pa_vel[a].y += direction.y * 10.0f / distance;
-                pb_vel[b].x += -direction.x * 10.0f / distance;
-                pb_vel[b].y += -direction.y * 10.0f / distance;
+            if (5.0f < distance && distance < 50.0f) {
+                pa_vel[a].x += direction.x * -10.0f / distance;
+                pa_vel[a].y += direction.y * -10.0f / distance;
+                pb_vel[b].x += -direction.x * -10.0f / distance;
+                pb_vel[b].y += -direction.y * -10.0f / distance;
             }
         }
     }
