55b532d486c3cc9297d5a5925a627b39e0a553b0
XnoobSpeakable <xnoobspeakable@gmail.com>
2026-07-21 17:06:53 +0300

    testing callbacks garbage

--- particlelife.cpp
+++ particlelife.cpp
@@ -1,6 +1,4 @@
-// SDL stuff
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+
 
 // Regular C++ stuff
 #include <iostream>
@@ -14,107 +12,123 @@
 #include <limits>
 #include <map>
 
-// Application running state
-bool running = true;
+/* points.c ... */
 
-// Rng stuff
-std::random_device rd;
-std::mt19937 eng(rd());
-std::uniform_int_distribution<> distr(INT32_MIN, INT32_MAX);
-int32_t rn() {
-    return distr(eng);
+/*
+ * This example creates an SDL window and renderer, and then draws some points
+ * to it every frame.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static Uint64 last_time = 0;
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define NUM_POINTS 500
+#define MIN_PIXELS_PER_SECOND 30  /* move at least this many pixels per second. */
+#define MAX_PIXELS_PER_SECOND 60  /* move this many pixels per second at most. */
+
+/* (track everything as parallel arrays instead of a array of structs,
+   so we can pass the coordinates to the renderer in a single function call.) */
+
+/* Points are plotted as a set of X and Y coordinates.
+   (0, 0) is the top left of the window, and larger numbers go down
+   and to the right. This isn't how geometry works, but this is pretty
+   standard in 2D graphics. */
+static SDL_FPoint points[NUM_POINTS];
+static float point_speeds[NUM_POINTS];
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    int i;
+
+    SDL_SetAppMetadata("Example Renderer Points", "1.0", "com.example.renderer-points");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/points", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    /* set up the data for a bunch of points. */
+    for (i = 0; i < SDL_arraysize(points); i++) {
+        points[i].x = SDL_randf() * ((float) WINDOW_WIDTH);
+        points[i].y = SDL_randf() * ((float) WINDOW_HEIGHT);
+        point_speeds[i] = MIN_PIXELS_PER_SECOND + (SDL_randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
+    }
+
+    last_time = SDL_GetTicks();
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-int main(int argc, char *argv[]) {
-    // DEVELOPER FLAGS
-    double targetFPS = 180.0;
-    double targetFrameTime = 1e9 / targetFPS;
-    bool limitFPS = true;
-    bool calculateFPS = true;
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
 
-    // Initialize Window
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    SDL_Window* window;
-    SDL_Surface* surface;
-    window = SDL_CreateWindow("Blind Factory", 640, 480, SDL_WINDOW_RESIZABLE);
-    surface = SDL_LoadBMP("./assets/textures/h.bmp");
-    SDL_BlitSurface(surface, nullptr, SDL_GetWindowSurface(window), nullptr);
-    SDL_UpdateWindowSurface(window);
-    SDL_DestroySurface(surface);
-    
-    // Initialize events and keyboard state
-    SDL_Event event;
-    const bool* keys = SDL_GetKeyboardState(nullptr);
-    
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    const Uint64 now = SDL_GetTicks();
+    const float elapsed = ((float) (now - last_time)) / 1000.0f;  /* seconds since last iteration */
+    int i;
 
-    // Initialize controls
-    struct MouseState{
-        bool left;
-        bool right;
-        bool middle;
-    };
-    MouseState mouseState;
-    struct KeyboardState {
-        bool up;
-        bool down;
-        bool enter;
-    };
-    KeyboardState keyboardState;
-
-    // Game loop
-    while(running) {
-        // Set up deltaTime
-        Uint64 pretime = SDL_GetTicksNS();
-        // Handle quit event and mouse click events
-        mouseState = {false, false, false};
-        keyboardState = {false, false, false};
-        while (SDL_PollEvent(&event)) {
-           if (event.type == SDL_EVENT_QUIT) {
-               running = false;
-           } else if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) {
-               if (event.button.button == SDL_BUTTON_LEFT) {
-                   mouseState.left = true;
-               }
-               if (event.button.button == SDL_BUTTON_RIGHT) {
-                   mouseState.right = true;
-               }
-               if (event.button.button == SDL_BUTTON_MIDDLE) {
-                   mouseState.middle = true;
-               }
-            } else if (event.type == SDL_EVENT_KEY_UP) {
-                if (event.key.key == SDLK_LEFT) {
-                    mouseState.left = true;
-                }
-                if (event.key.key == SDLK_RIGHT) {
-                    mouseState.right = true;
-                }
-                if (event.key.key == SDLK_UP) {
-                    mouseState.middle = true;
-                    keyboardState.up = true;
-                }
-                if (event.key.key == SDLK_DOWN) {
-                    mouseState.middle = true;
-                    keyboardState.down = true;
-                }
-                if(event.key.key == SDLK_KP_ENTER || event.key.key == SDLK_RETURN) {
-                    keyboardState.enter = true;
-                }
+    /* let's move all our points a little for a new frame. */
+    for (i = 0; i < SDL_arraysize(points); i++) {
+        const float distance = elapsed * point_speeds[i];
+        points[i].x += distance;
+        points[i].y += distance;
+        if ((points[i].x >= WINDOW_WIDTH) || (points[i].y >= WINDOW_HEIGHT)) {
+            /* off the screen; restart it elsewhere! */
+            if (SDL_rand(2)) {
+                points[i].x = SDL_randf() * ((float) WINDOW_WIDTH);
+                points[i].y = 0.0f;
+            } else {
+                points[i].x = 0.0f;
+                points[i].y = SDL_randf() * ((float) WINDOW_HEIGHT);
             }
-        }
-        
-        // FPS stuff
-        Uint64 posttime = SDL_GetTicksNS();
-        double deltatime = (posttime - pretime);
-        if (deltatime < targetFrameTime && limitFPS) {
-            SDL_DelayNS(targetFrameTime - deltatime);
-            posttime = SDL_GetTicksNS();
-            deltatime = (posttime - pretime);
-        }
-        if (calculateFPS) {
-            double FPS = 1e9 / deltatime;
-            std::cout << "FPS: " << FPS << std::endl;
+            point_speeds[i] = MIN_PIXELS_PER_SECOND + (SDL_randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
         }
     }
-    SDL_Quit();
-    return 0;
+
+    last_time = now;
+
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
+    SDL_RenderPoints(renderer, points, SDL_arraysize(points));  /* draw all the points! */
+
+    /* You can also draw single points with SDL_RenderPoint(), but it's
+       cheaper (sometimes significantly so) to do them all at once. */
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
